2a0f305edf879ce85d59464be91c21a3e1b6d0a6
12362aa <ahmedkhalifaw3@gmail.com>
2026-05-06 23:50:31 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const { WebSocketServer, WebSocket } = require('ws');
 const fs = require('fs');
 const path = require('path');
 const admin = require('firebase-admin');
+const { injectConfig } = require('./server-config');
 
 const PORT = process.env.PORT || 8081;
 
@@ -28,6 +29,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(injectConfig);
 
 const TOKENS_PATH = path.join(__dirname, 'tokens.json');
 
